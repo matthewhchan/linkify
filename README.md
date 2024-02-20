854db95a40e3c226f6f80127e8bb04c8fd0ac645
Matthew Chan <>
2024-02-20 12:09:05 -0800

    Use prettier for formatting.

--- README.md
+++ README.md
@@ -20,20 +20,20 @@ _Note: The underlying text is not modified. This plugin only changes the appeara
 
 To add a link pattern:
 
-- Go to the Options page for Linkify.
-- Click on the "Add New Link" button.
-- In the first text box, enter a regular expression.
-- In the second text box, write the URL you want the text to link to.[^1]
-- *(Optional)* Enter a CSS class in the third text box that will also be applied to any matching text.
-- Close the Options page.
+-   Go to the Options page for Linkify.
+-   Click on the "Add New Link" button.
+-   In the first text box, enter a regular expression.
+-   In the second text box, write the URL you want the text to link to.[^1]
+-   _(Optional)_ Enter a CSS class in the third text box that will also be applied to any matching text.
+-   Close the Options page.
 
 [^1]: The URL is allowed to include replacement patterns as described in [`String.prototype.replace`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#specifying_a_string_as_a_parameter).
 
-Here's what the Linkify *Options* page looks like:
+Here's what the Linkify _Options_ page looks like:
 
 <img width="790" alt="Linkify Options" src="https://github.com/matthewhchan/linkify/assets/37097379/486d4e9b-3e09-45be-bbe5-0903b5a74e84">
 
-Now, any text that matches a regular expression will be highlighted as though it were a link in *Live Preview* mode, and clicking on the text will open the link in a browser.
+Now, any text that matches a regular expression will be highlighted as though it were a link in _Live Preview_ mode, and clicking on the text will open the link in a browser.
 
 For example, if the regular expression is `@(\w+)` and the link pattern is `http://twitter.com/$1`, then any occurrence of `@obsdmd` in your notes will link to http://twitter.com/obsdmd. The same is true for any other name preceded by a `@` symbol.
 
--- main.ts
+++ main.ts
@@ -1,11 +1,16 @@
-import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
-import { Decoration, MatchDecorator, ViewPlugin, ViewUpdate } from "@codemirror/view";
+import { App, Plugin, PluginSettingTab, Setting } from "obsidian";
+import {
+	Decoration,
+	MatchDecorator,
+	ViewPlugin,
+	ViewUpdate,
+} from "@codemirror/view";
 import { RangeSet } from "@codemirror/state";
 
 interface LinkifyRule {
-	regexp: string,
-	link: string,
-	cssclass: string,
+	regexp: string;
+	link: string;
+	cssclass: string;
 }
 
 interface LinkifySettings {
@@ -22,7 +27,7 @@ const DEFAULT_SETTINGS: LinkifySettings = {
 		{
 			regexp: "g:([a-zA-Z0-9.-]*)",
 			link: "http://google.com/search?q=$1",
-			cssclass: ""
+			cssclass: "",
 		},
 		{
 			regexp: "gh:([a-zA-Z0-9./-]*)",
@@ -34,27 +39,34 @@ const DEFAULT_SETTINGS: LinkifySettings = {
 			link: "http://twitter.com/$1",
 			cssclass: "",
 		},
-	]
-}
+	],
+};
 
 const DEFAULT_NEW_RULE = {
 	regexp: "g\\/([a-zA-Z.-]*)",
 	link: "http://google.com/search?q=$1",
 	cssclass: "",
-}
+};
 
 // Creates a ViewPlugin from a LinkifyRule.
 function createViewPlugin(rule: LinkifyRule): LinkifyViewPlugin {
 	let decorator = new MatchDecorator({
 		regexp: new RegExp(rule.regexp, "g"),
-		decoration: Decoration.mark({ class: `cm-link linkified ${rule.cssclass}` }),
+		decoration: Decoration.mark({
+			class: `cm-link linkified ${rule.cssclass}`,
+		}),
 	});
-	return ViewPlugin.define(view => ({
-		decorations: decorator.createDeco(view),
-		update(u) { this.decorations = decorator.updateDeco(u, this.decorations) }
-	}), {
-		decorations: v => v.decorations
-	});
+	return ViewPlugin.define(
+		(view) => ({
+			decorations: decorator.createDeco(view),
+			update(u) {
+				this.decorations = decorator.updateDeco(u, this.decorations);
+			},
+		}),
+		{
+			decorations: (v) => v.decorations,
+		},
+	);
 }
 
 export default class Linkify extends Plugin {
@@ -74,16 +86,22 @@ export default class Linkify extends Plugin {
 		this.refreshExtensions();
 
 		// Linkify Reading mode.
-		this.registerMarkdownPostProcessor(this.markdownPostProcessor.bind(this));
+		this.registerMarkdownPostProcessor(
+			this.markdownPostProcessor.bind(this),
+		);
 
 		// Click to open the link.
-		this.registerDomEvent(document, 'click', evt => {
+		this.registerDomEvent(document, "click", (evt) => {
 			this.openLink(evt);
 		});
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData(),
+		);
 	}
 
 	async saveSettings() {
@@ -92,20 +110,31 @@ export default class Linkify extends Plugin {
 
 	// Creates new LinkifyViewPlugins and registers them.
 	refreshExtensions() {
-		this.viewPlugins.splice(0, this.viewPlugins.length, ... this.settings.rules.map(createViewPlugin));
+		this.viewPlugins.splice(
+			0,
+			this.viewPlugins.length,
+			...this.settings.rules.map(createViewPlugin),
+		);
 		this.app.workspace.updateOptions();
 	}
 
 	// Opens linkified text as a link.
 	openLink(evt: MouseEvent) {
-		if (evt.target instanceof HTMLSpanElement &&
-			evt.target.className.includes("cm-link linkified")) {
+		if (
+			evt.target instanceof HTMLSpanElement &&
+			evt.target.className.includes("cm-link linkified")
+		) {
 			let m = this.matchRule(evt.target.innerText);
 			if (m != null) {
 				// try to match internal link
-				const internalLinkMatch = m.link.match(/^\[\[([^|\]]*)(?:\|[^|\]]*)?\]\]$/);
+				const internalLinkMatch = m.link.match(
+					/^\[\[([^|\]]*)(?:\|[^|\]]*)?\]\]$/,
+				);
 				if (internalLinkMatch != null) {
-					this.app.workspace.openLinkText(internalLinkMatch.at(1), "");
+					this.app.workspace.openLinkText(
+						internalLinkMatch.at(1),
+						"",
+					);
 				} else {
 					window.open(m.link);
 				}
@@ -114,7 +143,9 @@ export default class Linkify extends Plugin {
 	}
 
 	// Returns the RegExp match and link for the given text.
-	matchRule(text: string): { match: RegExpMatchArray, link: string, cssclass: string } | null {
+	matchRule(
+		text: string,
+	): { match: RegExpMatchArray; link: string; cssclass: string } | null {
 		for (let rule of this.settings.rules) {
 			let regexp = new RegExp(rule.regexp);
 			let m = text.match(regexp);
@@ -123,7 +154,7 @@ export default class Linkify extends Plugin {
 					match: m,
 					link: m[0].replace(regexp, rule.link),
 					cssclass: rule.cssclass,
-				}
+				};
 			}
 		}
 
@@ -155,10 +186,14 @@ export default class Linkify extends Plugin {
 	// Converts matching text in the HTMLElement into links.
 	markdownPostProcessor(el: HTMLElement) {
 		if (el.firstChild instanceof Node) {
-			let walker = document.createTreeWalker(el.firstChild, NodeFilter.SHOW_TEXT, null);
+			let walker = document.createTreeWalker(
+				el.firstChild,
+				NodeFilter.SHOW_TEXT,
+				null,
+			);
 			let nodes: Node[] = [];
 			let node: Node;
-			while (node = walker.nextNode()) {
+			while ((node = walker.nextNode())) {
 				nodes.push(node);
 			}
 
@@ -183,51 +218,51 @@ class LinkifySettingTab extends PluginSettingTab {
 	display(): void {
 		const { containerEl } = this;
 		containerEl.empty();
-		containerEl.createEl('p', { text: 'Text matching the following regular expressions will be converted into links.' });
+		containerEl.createEl("p", {
+			text: "Text matching the following regular expressions will be converted into links.",
+		});
 
 		for (let [index, rule] of this.plugin.settings.rules.entries()) {
 			new Setting(containerEl)
 				.setDesc("RegExp/Link")
-				.addText(text => {
-					text.setValue(rule.regexp)
+				.addText((text) => {
+					text.setValue(rule.regexp);
 					text.inputEl.onblur = async () => {
 						rule.regexp = text.getValue();
 						await this.plugin.saveSettings();
 					};
 				})
-				.addText(text => {
+				.addText((text) => {
 					text.setValue(rule.link);
 					text.inputEl.onblur = async () => {
 						rule.link = text.getValue();
 						await this.plugin.saveSettings();
 					};
 				})
-				.addText(text => {
+				.addText((text) => {
 					text.setValue(rule.cssclass);
-					text.setPlaceholder("CSS Class")
+					text.setPlaceholder("CSS Class");
 					text.inputEl.onblur = async () => {
 						rule.cssclass = text.getValue();
 						await this.plugin.saveSettings();
 					};
 				})
-				.addButton(button => {
-					return button
-						.setIcon("trash")
-						.onClick(async () => {
-							this.plugin.settings.rules.splice(index, 1);
-							await this.plugin.saveSettings();
-							this.display();
-						});
+				.addButton((button) => {
+					return button.setIcon("trash").onClick(async () => {
+						this.plugin.settings.rules.splice(index, 1);
+						await this.plugin.saveSettings();
+						this.display();
+					});
 				});
 		}
 
-		new Setting(containerEl)
-			.addButton((button) => button
-				.setButtonText("Add New Link").onClick(async () => {
-					this.plugin.settings.rules.push({... DEFAULT_NEW_RULE});
-					await this.plugin.saveSettings();
-					this.display();
-				}));
+		new Setting(containerEl).addButton((button) =>
+			button.setButtonText("Add New Link").onClick(async () => {
+				this.plugin.settings.rules.push({ ...DEFAULT_NEW_RULE });
+				await this.plugin.saveSettings();
+				this.display();
+			}),
+		);
 	}
 
 	hide() {
